e6fb4e6cfb509ff5fd207d93ce1c027564b7cbe8
Emelianenkosa <emelianenko@tut.by>
2023-05-02 00:31:53 +0300

    feat: add language switching

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,49 +1,63 @@
-const KeyRowOne = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-const KeyRowTwo = ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'];
-const KeyRowThree = ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'];
-const KeyRowFour = ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '&#8593;', 'Shift'];
-const KeyRowFive = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '&#8592;', '&#8595;', '&#8594;'];
-const body = document.querySelector('body');
 
-function createKeyboard() {
-	const componentKeyBoard = createComponent('section', 'keyboard');
-	body.append(componentKeyBoard);
+const keyboard = {
+	En: {
+		1: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+		2: ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'],
+		3: ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'],
+		4: ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '&#8593;', 'Shift'],
+		5: ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '&#8592;', '&#8595;', '&#8594;'],
+	},
+	Ru: {
+		1: ['Ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+	2: ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '\\', 'Del'],
+	3: ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', "Э", 'Enter'],
+	4: ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '&#8593;', 'Shift'],
+	5: ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', 'Ctrl', '&#8592;', '&#8595;', '&#8594;'],
+	}
+};
+
+const body = document.querySelector('body');
+const componentKeyBoard = createComponent('section', 'keyboard');
+body.append(componentKeyBoard);
+
+
+function createKeyboard(lang) {
 
 	const rowOne = createComponent('div', 'row');
 	componentKeyBoard.append(rowOne);
-	for(let i=0; i < KeyRowOne.length; i++) {
-		if( KeyRowOne[i] === 'Backspace' ) {
-			rowOne.append(createComponentKey('button', 'key-double', KeyRowOne[i]));
+	for(let i=0; i < keyboard[lang][1].length; i++) {
+		if( keyboard[lang][1][i] === 'Backspace' ) {
+			rowOne.append(createComponentKey('button', 'key-double', keyboard[lang][1][i]));
 		} else {
-			rowOne.append(createComponentKey('button', 'key-single', KeyRowOne[i]));
+			rowOne.append(createComponentKey('button', 'key-single', keyboard[lang][1][i]));
 		}
 	}
 
 	const rowTwo = createComponent('div', 'row');
 	componentKeyBoard.append(rowTwo);
-	for(let i=0; i < KeyRowTwo.length; i++) {
-			let newComponent = createComponentKey('button', 'key-single', KeyRowTwo[i]);
+	for(let i=0; i < keyboard[lang][2].length; i++) {
+			let newComponent = createComponentKey('button', 'key-single', keyboard[lang][2][i]);
 			rowTwo.append(newComponent);
-			if(KeyRowTwo[i] === 'Del') {newComponent.setAttribute('id', 'delete')};
+			if(keyboard[lang][2][i] === 'Del') {newComponent.setAttribute('id', 'delete')};
 	}
 
 
 	const rowThree = createComponent('div', 'row');
 	componentKeyBoard.append(rowThree);
-	for(let i=0; i < KeyRowThree.length; i++) {
-		if( KeyRowThree[i] === 'CapsLock' || KeyRowThree[i] === 'Enter' ) {
-			rowThree.append(createComponentKey('button', 'key-double', KeyRowThree[i]));
+	for(let i=0; i < keyboard[lang][3].length; i++) {
+		if( keyboard[lang][3][i] === 'CapsLock' || keyboard[lang][3][i] === 'Enter' ) {
+			rowThree.append(createComponentKey('button', 'key-double', keyboard[lang][3][i]));
 		} else {
-			rowThree.append(createComponentKey('button', 'key-single', KeyRowThree[i]));
+			rowThree.append(createComponentKey('button', 'key-single', keyboard[lang][3][i]));
 		}
 	}
 
 	const rowFour = createComponent('div', 'row');
 	componentKeyBoard.append(rowFour);
-	for(let i=0; i < KeyRowFour.length; i++) {
-		let newComponentSingle = createComponentKey('button', 'key-single', KeyRowFour[i]);
-		let newComponentDouble = createComponentKey('button', 'key-double', KeyRowFour[i]);
-		if( KeyRowFour[i] === 'Shift') {
+	for(let i=0; i < keyboard[lang][4].length; i++) {
+		let newComponentSingle = createComponentKey('button', 'key-single', keyboard[lang][4][i]);
+		let newComponentDouble = createComponentKey('button', 'key-double', keyboard[lang][4][i]);
+		if( keyboard[lang][4][i] === 'Shift') {
 			rowFour.append(newComponentDouble);
 			if(i === 0) {newComponentDouble.setAttribute('id', 'shiftleft')};
 			if(i === 11) {newComponentDouble.setAttribute('id', 'shiftright')};
@@ -55,10 +69,10 @@ function createKeyboard() {
 
 	const rowFive = createComponent('div', 'row');
 	componentKeyBoard.append(rowFive);
-	for(let i=0; i < KeyRowFive.length; i++) {
-		let newComponentSingle = createComponentKey('button', 'key-single', KeyRowFive[i]);
-		let newComponentSpace = createComponentKey('button', 'key-space', KeyRowFive[i]);
-		if( KeyRowFive[i] === 'Space') {
+	for(let i=0; i < keyboard[lang][5].length; i++) {
+		let newComponentSingle = createComponentKey('button', 'key-single', keyboard[lang][5][i]);
+		let newComponentSpace = createComponentKey('button', 'key-space', keyboard[lang][5][i]);
+		if( keyboard[lang][5][i] === 'Space') {
 			rowFive.append(newComponentSpace);
 		} else {
 			rowFive.append(newComponentSingle);
@@ -73,8 +87,20 @@ function createKeyboard() {
 		}
 	}
 
+	const languageComponent = createComponent('p', 'language-text');
+	componentKeyBoard.append(languageComponent);
+	languageComponent.innerHTML = 'language switching: "Shift-Left" + "Alt-Left"'
+
 return componentKeyBoard;
 }
+
+function removeKeyboard() {
+	while (componentKeyBoard.firstChild) {
+		componentKeyBoard.removeChild(componentKeyBoard.firstChild);
+	}
+	return componentKeyBoard;
+}
+
 
 function createComponent(tagName, className) {
 	const element = document.createElement(tagName);
@@ -89,4 +115,5 @@ function createComponentKey(tagName, className, value) {
 	return element;
 }
 
-export { createKeyboard };
+
+export { createKeyboard, removeKeyboard };
--- main.js
+++ main.js
@@ -1,33 +1,43 @@
-import { createKeyboard } from './js/keyboard.js';
-//import { clickButtonInPhysicalKeyboard, removeActiveButtonInKeyboard } from './js/key-checked.js';
-//import * as S from './js/key-checked.js';
+import { createKeyboard, removeKeyboard } from './js/keyboard.js';
 
-createKeyboard();
-
+createKeyboard('En');
 
 //selected virtual key if click physical key
 const keyboard = document.querySelector('.keyboard');
 const button = document.querySelectorAll('button');
+const buttonShiftLeft = document.getElementById('shiftleft');
+const buttonAltLeft = document.getElementById('altleft');
+
+let lang = 'En';
 
 document.addEventListener('keydown', clickButtonInPhysicalKeyboard);
 document.addEventListener('keyup', removeActiveButtonInKeyboard);
 
 function clickButtonInPhysicalKeyboard(e) {
-	console.log(e.key, e.code);
 	for(let i=0; i < button.length; i++) {
-		console.log(e.code);
+		console.log(e.key, e.code);
 		if(button[i].id) {
 			if(e.key.toLowerCase() === button[i].id.toLowerCase() || e.code.toLowerCase() === button[i].id.toLowerCase() ) {
 				button[i].classList.add('key-active');
-				console.log('1');
 			}
 		};
 		if(!button[i].id) {
 			if( e.key.toLowerCase() === button[i].innerHTML.toLowerCase() || e.code.toLowerCase() === button[i].innerHTML.toLowerCase() ) {
 				button[i].classList.add('key-active');
-				console.log('2');
 			};
 		};
+	};
+	if(buttonShiftLeft.classList.contains('key-active') && buttonAltLeft.classList.contains('key-active')) {
+		if (lang === 'En') {
+			removeKeyboard();
+			createKeyboard('Ru');
+			lang = 'Ru';
+		} else {
+			removeKeyboard();
+			createKeyboard('En');
+			lang = 'En';
+		};
+		return;
 	};
 
 }
@@ -37,3 +47,6 @@ function removeActiveButtonInKeyboard() {
 			button[i].classList.remove('key-active');
 	};
 }
+
+
+
